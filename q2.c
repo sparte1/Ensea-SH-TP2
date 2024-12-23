6dd266d07fcf14a477d7be19d12daaa0e0d2dd97
ensea <ensea@StudentLab>
2024-12-23 01:56:29 +0100

    final q2

--- q2.c
+++ q2.c
@@ -37,7 +37,7 @@ struct addrinfo* resolve_server(const char *domain, const char *port) {
 
 int main(int argc, char *argv[]) {
     // Step 1: Validate the command-line arguments
-    validate_args(argc);
+    validate_args(argc, argv);
 
     // Step 2: Extract the domain and file name from arguments
     char *domain = argv[1];  // Server's address (e.g., "127.0.0.1")
